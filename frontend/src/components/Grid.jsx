1b4f06de061cafe4f3d7c4eeb5cb9071b3ab3c64
25mx101 <25mx101@MCA.PSG>
2025-11-12 17:00:35 +0530

    hero

--- frontend/src/components/Grid.jsx
+++ frontend/src/components/Grid.jsx
@@ -4,6 +4,9 @@ import img1 from "../assets/1.jpg";
 import img2 from "../assets/2.jpg";
 import img3 from "../assets/3.jpg";
 import img4 from "../assets/4.jpg";
+import img5 from '../assets/5.jpg';
+import img6 from '../assets/reverse.jpg';
+import img7 from '../assets/The Great Gatsby.jpg';
 
 
 function Grid() {
@@ -14,9 +17,9 @@ function Grid() {
         <div className="div2"><img src={img2} alt="" className="grid-img" /></div>
         <div className="div3"><img src={img3} alt="" className="grid-img" /></div>
         <div className="div4"><img src={img4} alt="" className="grid-img" /></div>
-        <div className="div5"><img src={img4} alt="" className="grid-img" /></div>
-        <div className="div6"><img src={img3} alt="" className="grid-img" /></div>
-        <div className="div7"><img src={img4} alt="" className="grid-img" /></div>
+        <div className="div5"><img src={img5} alt="" className="grid-img" /></div>
+        <div className="div6"><img src={img6} alt="" className="grid-img" /></div>
+        <div className="div7"><img src={img7} alt="" className="grid-img" /></div>
       </div>
     </>
   );
